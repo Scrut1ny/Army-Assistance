6396891ce829e007320e5213caa918aaf1d8140b
Scrut1ny <53458032+Scrut1ny@users.noreply.github.com>
2026-02-24 17:52:20 -0500

    Rename script and update metadata for SpeedMission

--- scripts/Auto-CA.js
+++ scripts/Auto-CA.js
@@ -1,69 +1,43 @@
 // ==UserScript==
-// @name         Cyber Awareness 2026 - Auto Answer
+// @name         SpeedMission Auto Answer
 // @namespace    http://tampermonkey.net/
-// @version      3.1
-// @description  Auto-selects correct answers and submits on the Cyber Awareness Challenge 2026 Speed Mission
-// @author       Scrut1ny
-// @match        https://cs.signal.army.mil/UserMngmt/CyberAwareness_2026/*
-// @match        https://cs.signal.army.mil/usermngmt/CyberAwareness_2026/*
+// @version      1.0
+// @description  Automatically answers all SpeedMission questions and submits the test
+// @author       You
+// @match        https://cs.signal.army.mil/UserMngmt/CyberAwareness_2026/pages/*
 // @grant        none
 // @run-at       document-idle
 // ==/UserScript==
 
-(function () {
+(function() {
     'use strict';
 
-    const SEL = '[x-data="speedMission"]';
-    const DELAY_SUBMIT = 400;
-    const DELAY_POLL = 800;
-    let lastQ = -1, timer;
+    const waitForRoot = () => {
+        const root = document.querySelector('[x-data="speedMission"]');
+        if (!root?._x_dataStack?.length) return setTimeout(waitForRoot, 50);
 
-    function click(el) {
-        if (!el) return;
-        const r = el.getBoundingClientRect();
-        const o = { bubbles: true, cancelable: true, clientX: r.x + r.width / 2, clientY: r.y + r.height / 2 };
-        for (const e of ['pointerdown', 'mousedown', 'pointerup', 'mouseup', 'click'])
-            el.dispatchEvent(new MouseEvent(e, o));
-    }
+        const state = root._x_dataStack[0], { sm } = state;
 
-    function getData(el) {
-        for (; el; el = el.parentElement)
-            if (el._x_dataStack)
-                for (const s of el._x_dataStack)
-                    if (s.sm?.qs) return s;
-                    return null;
-    }
+        const answerCurrent = () => {
+            const q = sm.qs[sm.num - 1];
+            if (!q?.a) return;
 
-    function run() {
-        const root = document.querySelector(SEL);
-        if (!root) return;
+            state.answer = q.a[1];
+            const isLast = sm.num === sm.qs.length;
 
-        const d = getData(root);
-        const q = d?.sm?.qs?.[d.sm.num - 1];
-        if (!q?.a || d.sm.num === lastQ) return;
+            if (isLast) {
+                const waitBtn = () => {
+                    const btn = document.querySelector('button.btn-blue[type="submit"]:not([disabled])');
+                    btn ? btn.click() : setTimeout(waitBtn, 20);
+                };
+                state.$nextTick(waitBtn);
+            } else {
+                state.$nextTick(() => { state.submitSm(); answerCurrent(); });
+            }
+        };
 
-        lastQ = d.sm.num;
-        const ans = q.a[1];
+        answerCurrent();
+    };
 
-        d.answer = ans;
-        for (const r of root.querySelectorAll('input[type="radio"]')) {
-            if (r.value === ans) { click(r); break; }
-        }
-
-        setTimeout(() => {
-            const btn = root.querySelector('button[type="submit"]:not([disabled])');
-            if (btn) click(btn);
-        }, DELAY_SUBMIT);
-    }
-
-    (function poll() {
-        const root = document.querySelector(SEL);
-        if (!root?._x_dataStack) return setTimeout(poll, 500);
-
-        new MutationObserver(() => { clearTimeout(timer); timer = setTimeout(run, 250); })
-        .observe(root, { childList: true, subtree: true, attributes: true });
-
-        setInterval(run, DELAY_POLL);
-        run();
-    })();
+    waitForRoot();
 })();
