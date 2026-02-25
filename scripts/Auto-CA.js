d7cdcc1d399a46eb20af1c30a8fff897ce18e0ef
Scrut1ny <53458032+Scrut1ny@users.noreply.github.com>
2026-02-24 19:21:49 -0500

    Refactor SpeedMission Auto Answer script
    Refactored the script to simplify the auto-answering logic for SpeedMission questions.

--- scripts/Auto-CA.js
+++ scripts/Auto-CA.js
@@ -1,43 +1,10 @@
-// ==UserScript==
-// @name         SpeedMission Auto Answer
-// @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Automatically answers all SpeedMission questions and submits the test
-// @author       You
-// @match        https://cs.signal.army.mil/UserMngmt/CyberAwareness_2026/pages/*
-// @grant        none
-// @run-at       document-idle
-// ==/UserScript==
+(function () {
+    const root = document.querySelector('[x-data="speedMission"]');
+    const state = root?._x_dataStack?.[0];
+    if (!state?.sm?.qs?.length) return;
 
-(function() {
-    'use strict';
-
-    const waitForRoot = () => {
-        const root = document.querySelector('[x-data="speedMission"]');
-        if (!root?._x_dataStack?.length) return setTimeout(waitForRoot, 50);
-
-        const state = root._x_dataStack[0], { sm } = state;
-
-        const answerCurrent = () => {
-            const q = sm.qs[sm.num - 1];
-            if (!q?.a) return;
-
-            state.answer = q.a[1];
-            const isLast = sm.num === sm.qs.length;
-
-            if (isLast) {
-                const waitBtn = () => {
-                    const btn = document.querySelector('button.btn-blue[type="submit"]:not([disabled])');
-                    btn ? btn.click() : setTimeout(waitBtn, 20);
-                };
-                state.$nextTick(waitBtn);
-            } else {
-                state.$nextTick(() => { state.submitSm(); answerCurrent(); });
-            }
-        };
-
-        answerCurrent();
-    };
-
-    waitForRoot();
+    state.sm.qs.forEach(q => {
+        state.answer = q.a;
+        state.submitSm();
+    });
 })();
